75ce714d9b57faf080648060df9a4b1e8e2b83b6
Antonio <l21590402@sjuanrio.tecnm.mx>
2025-02-11 15:56:49 -0600

    second commit

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app= Flask(__name__)
 def hola_mundo():
     return 'Soy team Ubuntu'
 
+@app.route('/alumnos')
+def alumnos():
+    return 'Aqui estan los datos de alumnos'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
